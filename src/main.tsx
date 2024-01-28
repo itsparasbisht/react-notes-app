eb0d9dff9d77c20e387918153d963dc7b9a1ac52
itsparasbisht <iamparasbisht@gmail.com>
2024-01-28 11:10:08 +0530

    initial setup

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Theme>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Theme>
+  </React.StrictMode>
+);
