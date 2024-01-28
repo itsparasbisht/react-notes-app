844a1846a52bccdc2e437da7ac0550f45d3d471b
itsparasbisht <iamparasbisht@gmail.com>
2024-01-28 15:57:02 +0530

    configured tailwind

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -4,37 +4,41 @@ import CreatableSelect from "react-select/creatable";
 function NoteForm() {
   return (
     <form>
-      <Flex direction="column" gap="3" style={{ maxWidth: 400 }}>
-        <div>
-          <label htmlFor="title">Title</label>
+      <Flex direction="row" gap="3">
+        <div style={{ flex: "1" }}>
+          <label htmlFor="title" className="mb-1 text-red-400">
+            Title
+          </label>
           <TextField.Input
             id="title"
             variant="soft"
             size="3"
             placeholder="Search the docs…"
           />
+        </div>
 
+        <div style={{ flex: "1" }}>
           <label htmlFor="tags">Tags</label>
           <CreatableSelect id="tags" isMulti />
         </div>
-        <div>
-          <label htmlFor="markdown">Body</label>
-          <TextArea
-            variant="soft"
-            size="3"
-            rows={10}
-            placeholder="Reply to comment…"
-          />
-        </div>
-        <div>
-          <Button type="submit" radius="large" variant="solid">
-            Submit
-          </Button>
-          <Button color="crimson" type="button" radius="large" variant="soft">
-            Cancel
-          </Button>
-        </div>
       </Flex>
+      <div>
+        <label htmlFor="markdown">Body</label>
+        <TextArea
+          variant="soft"
+          size="3"
+          rows={10}
+          placeholder="Reply to comment…"
+        />
+      </div>
+      <div>
+        <Button type="submit" radius="large" variant="solid">
+          Submit
+        </Button>
+        <Button color="crimson" type="button" radius="large" variant="soft">
+          Cancel
+        </Button>
+      </div>
     </form>
   );
 }
